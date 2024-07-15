36b13b9b73b1bc0eef55d1a38903df0dc0f7d943
fishmaster <a01022639410@gmail.com>
2024-07-16 01:51:04 +0900

    dad

---
+++ Assets/CountingStar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CountingStar : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI text;
+    private StarStorage starStorage;
+
+    private void Update()
+    {
+        if(starStorage.StarCount == 1)
+        {
+            text.text = "1";
+        }
+        else if(starStorage.StarCount == 2)
+        {
+            text.text = "2";
+        }
+        else if (starStorage.StarCount == 3)
+        {
+            text.text = "3";
+        }
+    }
+}
